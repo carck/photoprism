4bd87a177f4222dff3e6e46ca5945a0ecbdbb8e0
Carck <1028880+carck@users.noreply.github.com>
2023-10-06 07:40:34 +0800

    use contentless-delete fts5

--- internal/migrate/sqlite3/20231005-000001.sql
+++ internal/migrate/sqlite3/20231005-000001.sql
@@ -1,15 +1,14 @@
-CREATE VIRTUAL TABLE photo_search USING fts5(keywords, notes, content='', tokenize = 'simple');
+CREATE VIRTUAL TABLE photo_search USING fts5(keywords, notes, content='', tokenize = 'simple', contentless_delete=1);
 
 -- Triggers to keep the FTS index up to date.
 CREATE TRIGGER photos_ai AFTER INSERT ON details BEGIN
   INSERT INTO photo_search(rowid, keywords, notes) VALUES (new.photo_id, new.keywords, new.notes);
 END;
 CREATE TRIGGER photos_ad AFTER DELETE ON details BEGIN
-  INSERT INTO photo_search(photo_search, rowid, keywords, notes) VALUES('delete', old.photo_id, old.keywords, old.notes);
+  delete from photo_search where rowid = old.photo_id;
 END;
 CREATE TRIGGER photos_au AFTER UPDATE ON details BEGIN
-  INSERT INTO photo_search(photo_search, rowid, keywords, notes) VALUES('delete', old.photo_id, old.keywords, old.notes);
-  INSERT INTO photo_search(rowid, keywords, notes) VALUES (new.photo_id, new.keywords, new.notes);
+  update photo_search set keywords = new.keywords, notes = new.notes where rowid = new.photo_id;
 END;
 
 INSERT INTO photo_search(rowid, keywords, notes) select photo_id, keywords, notes from details;
