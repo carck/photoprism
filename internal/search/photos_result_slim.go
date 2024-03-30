f8b475d355d73f43faa5ea50d776bf2657ef2e03
carck <carck@foxmail.com>
2024-03-30 13:46:50 +0800

    add title to photo_slim

--- internal/search/photos_result_slim.go
+++ internal/search/photos_result_slim.go
@@ -7,9 +7,10 @@ import (
 type PhotoResultsSlim []PhotoSlim
 
 type PhotoSlim struct {
-	PhotoUID  string    `json:"UID"`
-	PhotoType string    `json:"Type"`
-	TakenAt   time.Time `json:"TakenAt"`
-	FileHash  string    `json:"Hash"`
-	PhotoName string    `json:"Name"`
+	PhotoUID   string    `json:"UID"`
+	PhotoType  string    `json:"Type"`
+	TakenAt    time.Time `json:"TakenAt"`
+	FileHash   string    `json:"Hash"`
+	PhotoName  string    `json:"Name"`
+	PhotoTitle string    `json:"Title"`
 }
