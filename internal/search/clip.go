3581ef60711152776282dbdf8784b02c707f423f
Carck <1028880+carck@users.noreply.github.com>
2026-03-11 02:29:54 +0000

    support similar cache

--- internal/search/clip.go
+++ internal/search/clip.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	gc "github.com/patrickmn/go-cache"
+	"github.com/photoprism/photoprism/pkg/rnd"
 )
 
 type clipResponse struct {
@@ -16,12 +17,32 @@ type clipResponse struct {
 }
 
 var clipCache = gc.New(300*time.Second, 60*time.Second)
+var clipServer = "http://127.0.0.1:9005"
+var clipHTTPGet = http.Get
 
-func SearchClip(query string, size int) (photos []uint, err error) {
-	if ids, found := clipCache.Get(query); found {
-		return ids.([]uint), nil
+func SearchClip(query string, size int) ([]uint, error) {
+	cacheKey := fmt.Sprintf("%s:%d", query, size)
+
+	if v, ok := clipCache.Get(cacheKey); ok {
+		if ids, ok := v.([]uint); ok {
+			return ids, nil
+		}
 	}
-	resp, err := http.Get("http://127.0.0.1:9005/search_text?query=" + url.QueryEscape(query) + "&top_k=" + strconv.Itoa(size))
+
+	params := url.Values{}
+	params.Set("top_k", strconv.Itoa(size))
+
+	if rnd.IsPPID(query, 'p') {
+		id, err := lookupPhotoID(query)
+		if err != nil {
+			return nil, err
+		}
+		params.Set("id", strconv.FormatUint(uint64(id), 10))
+	} else {
+		params.Set("query", query)
+	}
+
+	resp, err := clipHTTPGet(clipServer + "/search_text?" + params.Encode())
 	if err != nil {
 		return nil, err
 	}
@@ -31,11 +52,21 @@ func SearchClip(query string, size int) (photos []uint, err error) {
 		return nil, fmt.Errorf("clip: server error %s", resp.Status)
 	}
 
-	var result clipResponse
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+	var r clipResponse
+	if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
 		return nil, err
 	}
 
-	clipCache.Set(query, result.ImageIDs, gc.DefaultExpiration)
-	return result.ImageIDs, nil
+	clipCache.Set(cacheKey, r.ImageIDs, gc.DefaultExpiration)
+	return r.ImageIDs, nil
+}
+
+func lookupPhotoID(uid string) (uint, error) {
+	var id uint
+	err := UnscopedDb().
+		Table("photos").
+		Select("id").
+		Where("photo_uid = ?", uid).
+		Take(&id).Error
+	return id, err
 }
